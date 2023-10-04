39fdb4c48716c06e5f900ea1ef722b7ca94598b2
D-P-1018 <yvdeepika18@gmail.com>
2023-10-04 11:23:35 +0530

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Counter App with initial count of 0', () => {
+  const { getByText } = render(<App />);
+  const incrementButton = getByText('Increment');
+  const decrementButton = getByText('Decrement');
+  const countText = getByText('Count: 0');
+
+  expect(incrementButton).toBeInTheDocument();
+  expect(decrementButton).toBeInTheDocument();
+  expect(countText).toBeInTheDocument();
+});
+
+test('increment button increases count', () => {
+  const { getByText } = render(<App />);
+  const incrementButton = getByText('Increment');
+  const countText = getByText('Count: 0');
+
+  fireEvent.click(incrementButton);
+
+  expect(countText).toHaveTextContent('Count: 1');
+});
+
+test('decrement button decreases count', () => {
+  const { getByText } = render(<App />);
+  const decrementButton = getByText('Decrement');
+  const countText = getByText('Count: 0');
+
+  fireEvent.click(decrementButton);
+
+  expect(countText).toHaveTextContent('Count: -1');
 });
